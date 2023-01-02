f7078fcb4531c33bb3e366635935c0b663d4c8cf
Azazel32 <hugoreqalbion@gmail.com>
2023-01-02 15:23:38 +0100

    ewd

---
+++ console.js
@@ -0,0 +1,34 @@
+console.log("witaj siwecei");
+console.log(window);
+console.log(100000);
+console.table([1,2,3,4,5,6,7,8]);
+console.groupCollapsed("nazwa grupy");
+console.log("ala ma kota");
+console.log("nie ma kota");
+console.groupEnd();
+console.time("test");
+for (let i=0;i<+10000000;i++)
+{
+    ;
+}
+console.timeEnd("test");
+function test1(){
+    let l=0;
+    //debugger
+}
+test1();
+
+let text="ala";
+text="janek ma żyrafa";
+console.log({text});
+let a=0;
+a=10;
+const b=0;
+console.log(b);
+
+const tab=[1,2,3];
+tab[3]=4;
+tab.push(5,6);
+console.log(tab);
+
+
