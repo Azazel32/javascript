1b728d89e7fea2dc983854589d76d837ffc6e157
Azazel32 <hugoreqalbion@gmail.com>
2023-01-20 10:22:15 +0100

    zxcv

---
+++ alert.js
@@ -0,0 +1,15 @@
+function jezus (){
+    alert('To jest proste wywołanie okna')
+}
+function hal (){
+    alert('zxcvbn')
+}
+
+    a.querySelector('#c');
+    addEventListener('click', 't');
+function be(){
+    alert('kontynuj')
+}
+function ac(){
+    alert('wroc')
+}
