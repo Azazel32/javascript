91ba7a5ed817fcc1ebf11d9ca40dc3ad5e43195d
Azazel32 <hugoreqalbion@gmail.com>
2023-03-24 10:18:57 +0100

    jkhjkhjkhjkloojojpojp;opj;

---
+++ żarówka.js
@@ -0,0 +1,27 @@
+const wlancz=document.querySelector('#but1');
+const wylacz=document.querySelector('#but2');
+const radio=document.getElementsByClassName("radio");
+
+wlancz.addEventListener(type='click',function() {
+    document.querySelector('img').setAttribute('src','zarowka — kopia.jpg')
+    checkbox.toggleAttribute('checked')
+    radio.toggleAttribute('checked')
+})
+wylacz.addEventListener(type='click',function() {
+    document.querySelector('img').setAttribute('src','zarowka.jpg')
+    checkbox.toggleAttribute('checked')
+    radio.toggleAttribute('checked')
+})
+document.querySelector('#radio1').addEventListener(type='click',function (){
+    document.querySelector('img').setAttribute('src','zarowka — kopia.jpg')
+    checkbox.toggleAttribute('checked')
+})
+document.querySelector('#radio2').addEventListener(type='click',function (){
+    document.querySelector('img').setAttribute('src','zarowka.jpg')
+    checkbox.toggleAttribute('checked')
+
+})
+const checkbox=document.querySelector('#checkbox');
+
+
+
