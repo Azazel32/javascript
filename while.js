4874046508b29b85b0fa4b2f0c08401fa9582663
Azazel32 <hugoreqalbion@gmail.com>
2023-05-26 10:23:21 +0200

    funckej matematyczne zrobine

--- while.js
+++ while.js
@@ -10,27 +10,17 @@ document.querySelector('#generuj').addEventListener("click",function (){
     let w=1;
     while (w<=wiersz){
        const tabela_wiersz=document.createElement('tr');
-       tabela.appendChild(tabela_wiersz);
+
         while (k<=kolumna){
             const td=document.createElement('td')
-            tabela_wiersz.appendChild(td);
-
+            tabela.appendChild(td);
+            td.innerHTML=k+'|'+w;
             k++;
         }
         w++;
+        k=1;
+        tabela.appendChild(tabela_wiersz);
     }
-    k=1;
+
 })
 
-function populateTable(){
-    letters = ["a", "b", "c", "d", "e"]
-    numbers = ["1", "2", "3", "4", "5"]
-
-    for(let i = 0; i < numbers.length; i++){
-        document.getElementById("the-table").innerHTML += "<tr>"
-        for(let j = 0; j < letters.length; j++){
-            document.getElementById("the-table").innerHTML += "<td>" + letters[j] + numbers[i] + "</td>"
-        }
-        document.getElementById("the-table").innerHTML += "</tr>"
-    }
-}
